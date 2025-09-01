85bbd888c0963105cfa28404cecd63ef25f6723a
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-01 14:38:56 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -875,7 +875,7 @@ st.markdown(
     <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
         <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 10px;">
             <img src="https://cdn-icons-png.flaticon.com/512/3178/3178287.png" alt="Reference" style="width:20px; height:20px;">
-            <span>Dashboard Reference: <a href="https://flipsidecrypto.xyz/Azin/squid-bridge-kHpcoT" target="_blank">https://flipsidecrypto.xyz/Azin/squid-bridge-kHpcoT/</a></span>
+            <span>Dashboard Reference (Some of the code and charts were modified and rewritten): <a href="https://flipsidecrypto.xyz/Azin/squid-bridge-kHpcoT" target="_blank">https://flipsidecrypto.xyz/Azin/squid-bridge-kHpcoT/</a></span>
         </div>
         <div style="display: flex; align-items: center; gap: 10px;">
             <img src="https://pbs.twimg.com/profile_images/1856738793325268992/OouKI10c_400x400.jpg" alt="Flipside" style="width:25px; height:25px; border-radius: 50%;">
